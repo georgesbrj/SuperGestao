690cf3fd7d75d1f2d0e183cedac0105860fbbcd5
George Bezerra <george.bezerra@fgv.br>
2022-08-04 14:58:26 -0300

    input motivo contato dinamico

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
        
-        //var_dump($_POST);
-       return view('site.contato',["titulo" => "Contato"]);
+        //dd($request);
+
+        //1 forma de salvar dados 
+       /*  $contato = new SiteContato();          
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save(); */
+
+        //segunda forma de salvar dados
+        //$contato = new SiteContato();
+        //$contato->fill($request->all());        
+        //$contato->save();
+         //print_r($contato->getAttributes());
+
+
+         
+         $motivo_contatos = MotivoContato::all();
+       
+
+       return view('site.contato',["titulo" => "Contato",'motivo_contatos' => $motivo_contatos ]);
+    }
+
+
+
+    public function salvar(Request $request){
+          
+
+        dd($request);
+
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:200',
+        ]);
+         //SiteContato::create($request->all());
+         
     }
 }
